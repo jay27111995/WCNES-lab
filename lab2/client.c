0006fc1e4752cf290dd1a3a1e2ab08a817510164
Jayendra Prashanth <ejayen@gmail.com>
2021-02-01 23:47:40 +0100

    dsiji

--- lab2/client.c
+++ lab2/client.c
@@ -11,6 +11,9 @@
 #define LED_INT_ONTIME        CLOCK_SECOND
 #define ACCM_READ_INTERVAL    CLOCK_SECOND/100
 
+#define SIGNAL_INTRUSION_DETECTED 1
+#define SIGNAL_BUTTON_PRESSED 1
+
 /* Declare our "main" process, the client process*/
 PROCESS(client_process, "Clicker client");
 PROCESS(accel_process, "Accel process");
@@ -22,6 +25,7 @@ AUTOSTART_PROCESSES(&client_process, &accel_process, &led_process);
 
 
 static process_event_t ledOff_event;
+static process_event_t client_send_msg_event;
 
 /* LED handling process */
 static struct etimer ledETimer;
@@ -36,43 +40,59 @@ PROCESS_THREAD(led_process, ev, data) {
   PROCESS_END();
 }
 
-
+#if 0 
 void
 accm_movement_detected(uint8_t reg){
   
   leds_on(LEDS_BLUE);
   process_post(&led_process, ledOff_event, NULL);
-  //process_post(&client_process, clientSendMsg_event, NULL);  
+  process_post(&client_process, client_send_msg_event, NULL);  
 
   //printf("~~[%u] Freefall detected! (0x%02X) -- ", ((uint16_t) clock_time())/128, reg);
   //print_int(reg);
 }
+#endif 
 
 /* Accelerometer process */
 static struct etimer et;
+
+
 PROCESS_THREAD(accel_process, ev, data) {
   PROCESS_BEGIN();
   {
-    int16_t x, y, z;
+    int16_t x;
+    static int16_t x_old; 
+    const int16_t error_val = 50; 
 
     /* Register the event used for lighting up an LED when interrupt strikes. */
     ledOff_event = process_alloc_event();
+    client_send_msg_event = process_alloc_event();
 
     /* Start and setup the accelerometer with default values, eg no interrupts enabled. */
     accm_init();
+    x_old = accm_read_axis(X_AXIS);
 
+#if 0 
     /* Register the callback functions for each interrupt */
     ACCM_REGISTER_INT1_CB(accm_movement_detected);
 
     /* Set what strikes the corresponding interrupts. Several interrupts per pin is 
       possible. For the eight possible interrupts, see adxl345.h and adxl345 datasheet. */
     accm_set_irq(ADXL345_INT_ACTIVITY, ADXL345_INT_DISABLE);
+#endif 
 
     while (1) {
 	    x = accm_read_axis(X_AXIS);
-	    y = accm_read_axis(Y_AXIS);
-	    z = accm_read_axis(Z_AXIS);
-	    printf("x: %d y: %d z: %d\n", x, y, z);
+	    printf("x: %d \n", x);
+        
+        if ((x_old + error_val) > x) {
+            process_post(&client_process, client_send_msg_event, NULL);  
+
+            leds_on(LEDS_BLUE);
+            process_post(&led_process, ledOff_event, NULL);
+        }
+
+        x_old = x;
 
       etimer_set(&et, ACCM_READ_INTERVAL);
       PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
@@ -100,12 +120,14 @@ static void recv(const void *data, uint16_t len,
 PROCESS_THREAD(client_process, ev, data) {
 	PROCESS_BEGIN();
 
+    static uint8_t msg1 = SIGNAL_INTRUSION_DETECTED;
 #if 0 
     static char msg1[] = "hej";
     static char msg2[] = "hej";
 
 	/* Activate the button sensor. */
 	SENSORS_ACTIVATE(button_sensor);
+#endif 
 
 	/* Initialize NullNet */
 	nullnet_buf = (uint8_t *)&payload;
@@ -120,19 +142,21 @@ PROCESS_THREAD(client_process, ev, data) {
 		 * event. In the case of a sensors_event, data will
 		 * point to the sensor that caused the event.
 		 * Here we wait until the button was pressed. */
+#if 0 
 		PROCESS_WAIT_EVENT_UNTIL(ev == sensors_event &&
 			data == &button_sensor);
+#endif 
+		PROCESS_WAIT_EVENT_UNTIL(ev == client_send_msg_event); 
 
 		leds_toggle(LEDS_RED);
 		/* Copy the string "hej" into the packet buffer. */
-		memcpy(nullnet_buf, &payload, sizeof(payload));
-    nullnet_len = sizeof(payload);
+		memcpy(nullnet_buf, &msg1, sizeof(msg1));
+    nullnet_len = sizeof(msg1);
 
 		/* Send the content of the packet buffer using the
 		 * broadcast handle. */
 		NETSTACK_NETWORK.output(NULL);
 	}
 
-#endif 
 	PROCESS_END();
 }
